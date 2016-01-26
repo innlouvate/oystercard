3e1ddc29780ff1353152355dc3bd33d9d884635c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-26 16:10:36 +0000

    Journey class

---
+++ spec/journey_spec.rb
@@ -0,0 +1,20 @@
+require 'journey'
+
+describe Journey do
+  subject(:journey) {described_class.new(entry_station)}
+  let(:entry_station) { double :entry_station }
+  let(:exit_station) { double :exit_station }
+  let(:oystercard)  { double :oystercard}
+
+  describe 'creates new journey' do
+    it 'should have an entry station' do
+      expect(journey.entry_station).to be_truthy
+    end
+
+    describe 'journey complete' do
+      it 'checks journey is not complete' do
+        expect(journey.complete_journey?).to eq false
+      end
+    end
+  end
+end
