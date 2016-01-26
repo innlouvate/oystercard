52a8880300b2d378c648e349f7988cb8cad23968
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-26 16:09:14 +0000

    journey class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,11 @@ require_relative 'station'
 
 class Oystercard
 
-  attr_reader :balance, :entry_station, :journey_list
+  attr_reader :balance, :journey_list, :entry_station
   BALANCE_MAX = 90
   FARE_MIN = 1
-
   def initialize
     @balance = 0
-    @in_use = false
     @journey_list = []
   end
 
@@ -19,18 +17,17 @@ class Oystercard
 
   def touch_in(station)
     raise 'Please top up your card.' if @balance < FARE_MIN
-    @entry_station = station
+    # @entry_station = station
+
   end
 
   def touch_out(station)
     deduct(FARE_MIN)
     journey_capture(station)
-    @entry_station = nil
+    # @entry_station = nil
   end
+  #
 
-  def in_journey?
-    @entry_station != nil
-  end
 
   private
 
@@ -39,7 +36,7 @@ class Oystercard
   end
 
   def journey_capture(exitstation)
-    journey = { @entry_station => exitstation}
+    journey = { @entry_station => exitstation }
      @journey_list << journey
   end
 end
